2185fd2a78bc8e71434915c970a1508c7515f54f
jv.lee@foxmail.com <jv.lee@foxmail.com>
2020-04-11 06:00:21 +0800

    添加LineButtonView自定义属性

--- Pioneer-Android/app/src/main/java/com/lee/pioneer/view/widget/LineButtonView.kt
+++ Pioneer-Android/app/src/main/java/com/lee/pioneer/view/widget/LineButtonView.kt
@@ -20,6 +20,19 @@ import com.lee.pioneer.R
  */
 class LineButtonView : ConstraintLayout {
 
+    private var iconSize: Int = 0
+    private var itemTextSize: Float = 0F
+    private var itemTextMargin: Int = 0
+    private var itemTextColor: Int = 0
+    private lateinit var itemText: String
+    private var leftDrawableId = 0
+    private var rightDrawableId = 0
+
+
+    private lateinit var leftDrawable: ImageView
+    private lateinit var rightDrawable: ImageView
+
+
     constructor(context: Context) : this(context, null, 0)
     constructor(context: Context, attributes: AttributeSet) : this(context, attributes, 0)
     constructor(context: Context, attributes: AttributeSet?, defStyleAttr: Int) : super(
@@ -27,13 +40,36 @@ class LineButtonView : ConstraintLayout {
         attributes,
         defStyleAttr
     ) {
-        initAttribute()
+        initAttribute(attributes!!)
         initStyle()
         initView()
     }
 
-    private fun initAttribute() {
+    private fun initAttribute(attrs: AttributeSet) {
 
+        context.obtainStyledAttributes(attrs, R.styleable.LineButtonView).run {
+            iconSize =
+                getDimensionPixelSize(
+                    R.styleable.LineButtonView_iconSize,
+                    resources.getDimensionPixelSize(R.dimen.view_line_icon_size)
+                )
+            itemTextSize = getDimension(
+                R.styleable.LineButtonView_itemTextSize,
+                SizeUtil.dimensToSp(context, resources.getDimension(R.dimen.view_line_text_size))
+            )
+            itemTextMargin = getDimensionPixelSize(
+                R.styleable.LineButtonView_itemTextMargin,
+                resources.getDimension(R.dimen.view_line_text_margin).toInt()
+            )
+            itemTextColor = getColor(
+                R.styleable.LineButtonView_itemTextColor,
+                ContextCompat.getColor(context, R.color.colorThemeAccent)
+            )
+            leftDrawableId = getResourceId(R.styleable.LineButtonView_leftDrawable, 0)
+            rightDrawableId = getResourceId(R.styleable.LineButtonView_rightDrawable, 0)
+
+            recycle()
+        }
     }
 
     private fun initStyle() {
@@ -43,18 +79,18 @@ class LineButtonView : ConstraintLayout {
     }
 
     private fun initView() {
-        val iconSize = resources.getDimension(R.dimen.view_line_icon_size).toInt()
-        val textMargin = resources.getDimension(R.dimen.view_line_text_margin).toInt()
-        val textSize = SizeUtil.dimensToSp(context,resources.getDimension(R.dimen.view_line_text_size))
-        val ivIcon = ImageView(context)
-        ivIcon.run {
+
+        leftDrawable = ImageView(context)
+        leftDrawable.run {
             layoutParams = LayoutParams(iconSize, iconSize)
             updateLayoutParams<ConstraintLayout.LayoutParams> {
                 startToStart = 0
                 topToTop = 0
                 bottomToBottom = 0
             }
-            setImageResource(R.mipmap.ic_launcher)
+            if (leftDrawableId != 0) {
+                setImageResource(leftDrawableId)
+            }
             addView(this)
         }
 
@@ -66,25 +102,28 @@ class LineButtonView : ConstraintLayout {
                 startToStart = 0
                 topToTop = 0
                 bottomToBottom = 0
-                leftMargin = textMargin
+                leftMargin = itemTextMargin
             }
             setTextSize(
                 TypedValue.COMPLEX_UNIT_SP,
-                textSize)
-            setTextColor(ContextCompat.getColor(context, R.color.colorThemeAccent))
-            text = "历史记录"
+                itemTextSize
+            )
+            setTextColor(itemTextColor)
+            text = itemText
             addView(this)
         }
 
-        val ivArrow = ImageView(context)
-        ivArrow.run {
+        rightDrawable = ImageView(context)
+        rightDrawable.run {
             layoutParams = LayoutParams(iconSize, iconSize)
             updateLayoutParams<ConstraintLayout.LayoutParams> {
                 endToEnd = 0
                 topToTop = 0
                 bottomToBottom = 0
             }
-            setImageResource(R.drawable.vector_arrow)
+            if (rightDrawableId != 0) {
+                setImageResource(rightDrawableId)
+            }
             addView(this)
         }
     }
