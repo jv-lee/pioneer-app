a8c82d2db50db65d7252c44b4ec5739a9aeb04a5
lijiawei <lijiawei@52zhifan.net>
2020-04-17 18:20:24 +0800

    添加rv glide resume扩展函数.

--- Pioneer-Android/app/src/main/java/ViewExtensions.kt
+++ Pioneer-Android/app/src/main/java/ViewExtensions.kt
@@ -1,5 +1,7 @@
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
 /**
  * @author jv.lee
@@ -8,4 +10,13 @@ import androidx.fragment.app.Fragment
  */
 fun Fragment.toast2(message: CharSequence, duration: Int = Toast.LENGTH_SHORT) {
     Toast.makeText(activity, message, duration).show()
+}
+
+fun RecyclerView.glideEnable(){
+    addOnScrollListener(object:RecyclerView.OnScrollListener(){
+        override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+            super.onScrollStateChanged(recyclerView, newState)
+            if(getContext() != null) Glide.with(getContext()).resumeRequests()
+        }
+    })
 }
