0606991c3d6976709bd17181a245a55e0311a693
jv.lee <jv.lee@foxmail.com>
2020-04-24 11:19:35 +0800

    添加意见反馈commit逻辑.

--- Pioneer-Android/app/src/main/java/com/lee/pioneer/view/fragment/FeedbackFragment.kt
+++ Pioneer-Android/app/src/main/java/com/lee/pioneer/view/fragment/FeedbackFragment.kt
@@ -1,9 +1,11 @@
 package com.lee.pioneer.view.fragment
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import com.lee.library.base.BaseFragment
 import com.lee.pioneer.R
 import com.lee.pioneer.databinding.FragmentFeedbackBinding
+import com.lee.pioneer.viewmodel.FeedbackViewModel
 
 /**
  * @author jv.lee
@@ -11,14 +13,21 @@ import com.lee.pioneer.databinding.FragmentFeedbackBinding
  * @description MeFragment ChildPage -> 反馈页面
  */
 class FeedbackFragment :
-    BaseFragment<FragmentFeedbackBinding, ViewModel>(R.layout.fragment_feedback, null) {
+    BaseFragment<FragmentFeedbackBinding, FeedbackViewModel>(
+        R.layout.fragment_feedback,
+        FeedbackViewModel::class.java
+    ) {
 
     override fun bindView() {
-
     }
 
     override fun bindData() {
+        binding.vm = viewModel
 
+        viewModel.toastStrObserver.observe(this@FeedbackFragment, Observer {
+            toast(it)
+            if (it == getString(R.string.feedback_success)) findNavController().popBackStack()
+        })
     }
 
 }
---
+++ Pioneer-Android/app/src/main/java/com/lee/pioneer/viewmodel/FeedbackViewModel.kt
@@ -0,0 +1,45 @@
+package com.lee.pioneer.viewmodel
+
+import android.app.Application
+import android.text.TextUtils
+import androidx.databinding.ObservableField
+import androidx.lifecycle.MutableLiveData
+import com.lee.library.mvvm.base.BaseViewModel
+import com.lee.library.widget.interadap.TextWatcherAdapter
+import com.lee.pioneer.App
+import com.lee.pioneer.R
+
+/**
+ * @author jv.lee
+ * @date 2020/4/24
+ * @description
+ */
+class FeedbackViewModel(application: Application) : BaseViewModel(application) {
+
+    private val contentField by lazy { ObservableField<String>("") }
+    private val linkField by lazy { ObservableField<String>("") }
+    val toastStrObserver by lazy { MutableLiveData<String>() }
+
+    val etContentWatcher = object : TextWatcherAdapter() {
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            contentField.set(s.toString())
+        }
+
+    }
+
+    val etLinkWatcher = object : TextWatcherAdapter() {
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            super.onTextChanged(s, start, before, count)
+            linkField.set(s.toString())
+        }
+    }
+
+    fun commit() {
+        if (TextUtils.isEmpty(contentField.get()) || TextUtils.isEmpty(linkField.get())) {
+            toastStrObserver.value = getApplication<App>().getString(R.string.feedback_empty)
+            return
+        }
+        toastStrObserver.value = getApplication<App>().getString(R.string.feedback_success)
+    }
+
+}
