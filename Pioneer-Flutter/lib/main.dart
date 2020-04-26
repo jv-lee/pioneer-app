b5479f6422ef60ada5b51be44304fce8f1830990
jv.lee@foxmail.com <jv.lee@foxmail.com>
2020-04-26 23:51:00 +0800

    init pioneer-flutter

---
+++ Pioneer-Flutter/lib/main.dart
@@ -0,0 +1,54 @@
+import 'dart:io';
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(MyApp());
+  if (Platform.isAndroid) {
+    SystemUiOverlayStyle systemUiOverlayStyle =
+        SystemUiOverlayStyle(statusBarColor: Colors.transparent);
+    SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
+  }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding:
+            EdgeInsets.only(top: MediaQueryData.fromWindow(window).padding.top),
+        child: Container(
+          color: Colors.blue,
+          child: Center(
+            child: Text('content'),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// 获取状态栏高度 EdgeInsets.only(top: MediaQueryData.fromWindow(window).padding.top),
