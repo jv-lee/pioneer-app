6f0394d492eb8bc2a0130351472de42408c52fa4
jv.lee@foxmail.com <jv.lee@foxmail.com>
2020-04-29 00:30:10 +0800

    App/MainActivity 替换包路径重新上传.

---
+++ Pioneer-Flutter/android/app/src/main/kotlin/com/lee/pioneer/flutter/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.lee.pioneer.flutter
+
+import android.os.Bundle
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity : FlutterActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
+
+}
