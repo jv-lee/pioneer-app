3decace68a4799a311dc3619371af89015af8fba
jv.lee <jv.lee@foxmail.com>
2020-05-14 14:29:09 +0800

    添加SuperListView测试组件.

---
+++ Pioneer-Flutter/lib/test/super_list_view_test.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:pioneer_flutter/view/widget/status/status.dart';
+import 'package:pioneer_flutter/view/widget/status/status_controller.dart';
+import 'package:pioneer_flutter/view/widget/status/super_list_view.dart';
+
+/// @author jv.lee
+/// @date 2020/5/14
+/// @description  SuperListView测试案例
+class SuperListViewTest extends StatefulWidget {
+  SuperListViewTest({Key key}) : super(key: key);
+
+  @override
+  _SuperListViewTestState createState() => _SuperListViewTestState();
+}
+
+class _SuperListViewTestState extends State<SuperListViewTest> {
+  StatusController _statusController;
+
+  @override
+  void initState() {
+    super.initState();
+    _statusController = StatusController(
+        pageStatus: PageStatus.error, itemStatus: ItemStatus.empty);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SuperListView(
+      statusController: _statusController,
+      itemCount: 30,
+      onPageReload: () {
+        _statusController.pageComplete();
+        _statusController.itemError();
+      },
+      onItemReload: () {
+        _statusController.itemComplete();
+      },
+      headerChildren: <Widget>[
+        Container(
+          color: Colors.orange,
+          height: 30,
+        ),
+        Container(
+          color: Colors.yellow,
+          height: 30,
+        ),
+        Container(
+          color: Colors.green,
+          height: 30,
+        ),
+        Container(
+          color: Colors.pink,
+          height: 30,
+        ),
+      ],
+      footerChildren: <Widget>[
+        Container(
+          color: Colors.blue,
+          height: 50,
+        ),
+        Container(
+          color: Colors.brown,
+          height: 50,
+        ),
+      ],
+      itemBuilder: (context, index) {
+        return Container(
+          margin: EdgeInsets.only(bottom: 10),
+          color: Colors.red,
+          padding: EdgeInsets.all(10),
+          child: Center(
+            child: Text('this is item $index'),
+          ),
+        );
+      },
+    );
+  }
+}
--- Pioneer-Flutter/lib/view/page/girl/girl.dart
+++ Pioneer-Flutter/lib/view/page/girl/girl.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:pioneer_flutter/view/widget/status/status.dart';
-import 'package:pioneer_flutter/view/widget/status/status_controller.dart';
-import 'package:pioneer_flutter/view/widget/status/super_list_view.dart';
+import 'package:pioneer_flutter/test/super_list_view_test.dart';
 
 /// @author jv.lee
 /// @date 2020/4/30
@@ -14,65 +12,8 @@ class GirlPage extends StatefulWidget {
 }
 
 class GirlState extends State<GirlPage> {
-  StatusController _statusController;
-
-  @override
-  void initState() {
-    super.initState();
-    _statusController = StatusController(
-        pageStatus: PageStatus.error, itemStatus: ItemStatus.empty);
-  }
-
   @override
   Widget build(BuildContext context) {
-    return SuperListView(
-      statusController: _statusController,
-      itemCount: 30,
-      onPageReload: () {
-        _statusController.pageComplete();
-        _statusController.itemError();
-      },
-      onItemReload: () {
-        _statusController.itemComplete();
-      },
-      headerChildren: <Widget>[
-        Container(
-          color: Colors.orange,
-          height: 30,
-        ),
-        Container(
-          color: Colors.yellow,
-          height: 30,
-        ),
-        Container(
-          color: Colors.green,
-          height: 30,
-        ),
-        Container(
-          color: Colors.pink,
-          height: 30,
-        ),
-      ],
-      footerChildren: <Widget>[
-        Container(
-          color: Colors.blue,
-          height: 50,
-        ),
-        Container(
-          color: Colors.brown,
-          height: 50,
-        ),
-      ],
-      itemBuilder: (context, index) {
-        return Container(
-          margin: EdgeInsets.only(bottom: 10),
-          color: Colors.red,
-          padding: EdgeInsets.all(10),
-          child: Center(
-            child: Text('this is item $index'),
-          ),
-        );
-      },
-    );
+    return SuperListViewTest();
   }
 }
