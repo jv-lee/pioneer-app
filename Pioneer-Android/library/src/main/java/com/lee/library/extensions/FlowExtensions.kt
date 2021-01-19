081c514495b7657eb005b4b0bf1bf1ae057768f6
jv.lee <jv.lee@foxmail.com>
2021-01-19 10:50:10 +0800

    优化LiveData异常处理.

--- Pioneer-Android/library/src/main/java/com/lee/library/extensions/FlowExtensions.kt
+++ Pioneer-Android/library/src/main/java/com/lee/library/extensions/FlowExtensions.kt
@@ -10,7 +10,6 @@ import kotlinx.coroutines.flow.*
  * @description
  */
 suspend fun <T> Flow<T>.bindLive(liveData: BaseLiveData<T>) {
-    Dispatchers
     catch {
         liveData.throwMessage(it)
     }.collect {
