460d864b7687eb89df43278462245adaedb1e8b2
lijiawei11 <lijiawei11@baidu.com>
2021-09-03 17:40:18 +0800

    update code.

--- Pioneer-Android/app/src/main/java/com/lee/pioneer/tools/BindingAdapter.kt
+++ Pioneer-Android/app/src/main/java/com/lee/pioneer/tools/BindingAdapter.kt
@@ -1,8 +1,6 @@
 package com.lee.pioneer.tools
 
-import android.widget.CompoundButton
 import android.widget.EditText
-import android.widget.Switch
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 
@@ -11,12 +9,7 @@ import androidx.databinding.BindingAdapter
  * @date 2020/4/7
  * @description
  */
-@BindingAdapter("android:editActionListener")
+@BindingAdapter("binding:editActionListener")
 fun setEditActionListener(view: EditText, listener: TextView.OnEditorActionListener) {
     view.setOnEditorActionListener(listener)
-}
-
-@BindingAdapter("android:checkedChangeListener")
-fun setOnCheckedChangeListener(view:Switch,listener:CompoundButton.OnCheckedChangeListener){
-    view.setOnCheckedChangeListener(listener)
 }
