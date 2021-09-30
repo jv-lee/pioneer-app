854b0c8f196fca86aef11b465edaa607570bcd7f
lijiawei11 <lijiawei11@baidu.com>
2021-09-30 10:12:45 +0800

    update build code.

---
+++ Pioneer-Android/build-config/src/main/java/com/lee/pioneer/config/BuildConfig.kt
@@ -0,0 +1,51 @@
+package com.lee.pioneer.config
+
+/**
+ * @author jv.lee
+ * @data 2021/9/28
+ * @description
+ */
+object BuildConfig {
+    const val applicationId = "com.lee.pioneer"
+
+    const val compileSdk = 30
+    const val minSdk = 19
+    const val targetSdk = 30
+    const val versionCode = 1
+    const val versionName = "1.0.0"
+
+    const val multiDex = true
+
+    const val SUPPORT_LIBRARY_VECTOR_DRAWABLES = true
+
+    const val TEST_INSTRUMENTATION_RUNNER = "androidx.test.runner.AndroidJUnitRunner"
+    val TEST_INSTRUMENTATION_RUNNER_ARGUMENTS = mapOf(
+        "leakcanary.FailTestOnLeakRunListener" to "listener"
+    )
+}
+
+object BuildDebug {
+    const val minifyEnabled = false
+    const val zipAlignEnabled = false
+    const val baseApi = "https://gank.io/api/v2/"
+
+    object Signing {
+        const val storeFile = "../pioneer.jks"
+        const val storePassword = "123456"
+        const val keyAlias = "pioneer"
+        const val keyPassword = "123456"
+    }
+}
+
+object BuildRelease {
+    const val minifyEnabled = true
+    const val zipAlignEnabled = true
+    const val baseApi = "https://gank.io/api/v2/"
+
+    object Signing {
+        const val storeFile = "../pioneer.jks"
+        const val storePassword = "123456"
+        const val keyAlias = "pioneer"
+        const val keyPassword = "123456"
+    }
+}
---
+++ Pioneer-Android/build-config/src/main/java/com/lee/pioneer/config/DependencyPlugin.kt
@@ -0,0 +1,15 @@
+package com.lee.pioneer.config
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * @author jv.lee
+ * @data 2021/9/29
+ * @description
+ */
+class DependencyPlugin : Plugin<Project> {
+    override fun apply(project: Project) {
+
+    }
+}
