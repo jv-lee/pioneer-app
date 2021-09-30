518e58f0977505c938d90cbf174178db273da112
lijiawei11 <lijiawei11@baidu.com>
2021-09-30 16:17:54 +0800

    update build.

--- Pioneer-Android/buildSrc/build.gradle.kts
+++ Pioneer-Android/buildSrc/build.gradle.kts
@@ -3,15 +3,5 @@ plugins {
 }
 
 repositories {
-    google()
     mavenCentral()
-    maven("https://jitpack.io")
-    maven("https://maven.aliyun.com/repository/google")
-    maven("https://maven.aliyun.com/repository/jcenter/")
-}
-
-dependencies {
-    implementation("com.android.tools.build:gradle:7.0.0")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
-    implementation("androidx.navigation:navigation-safe-args-gradle-plugin:2.4.0-alpha06")
 }
