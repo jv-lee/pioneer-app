ad521081c2e090b73765be3c5baa64ea1eee15db
lijiawei11 <lijiawei11@baidu.com>
2021-10-25 15:37:18 +0800

    update dark mode.

--- Pioneer-Android/library/common/src/main/java/com/lee/pioneer/library/common/tools/GlideTools.kt
+++ Pioneer-Android/library/common/src/main/java/com/lee/pioneer/library/common/tools/GlideTools.kt
@@ -7,6 +7,7 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.Priority
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
+import com.lee.library.tools.DarkModeTools
 import com.lee.pioneer.library.common.R
 
 /**
@@ -47,7 +48,11 @@ class GlideTools {
     fun loadImage(
         path: String?,
         imageView: ImageView,
-        @DrawableRes placeholderResId: Int = R.mipmap.ic_picture_placeholder
+        @DrawableRes placeholderResId: Int =
+            if (DarkModeTools.get().isDarkTheme())
+                R.mipmap.ic_picture_placeholder_night
+            else
+                R.mipmap.ic_picture_placeholder
     ) {
         val request = Glide.with(imageView.context)
             .load(http2https(path))
